cfa879441a53727faa5edd699ea5201ed857bd01
Jaehun Kim <42717042+wogns3623@users.noreply.github.com>
2025-01-03 00:13:38 +0900

    Update init.sh
    Add todo

--- init.sh
+++ init.sh
@@ -23,3 +23,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat
+
+# TODO: Add obsidian git sync
