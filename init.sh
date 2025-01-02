ae664af23e2decb5a044cb9d418d65580bf6ade0
Jaehun Kim <42717042+wogns3623@users.noreply.github.com>
2025-01-02 18:16:17 +0900

    Update init.sh
    Add manual git install before restore

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/wogns3623/dotfiles/HEAD/init.sh)"
+
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install git
+
+# initialize git into home directory
+git init
+git remote add origin "https://github.com/wogns3623/dotfiles.git"
+git fetch
+git reset origin/main
+git stash
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -9,10 +20,3 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat
-
-# initialize git into home directory
-git init
-git remote add origin "$1"
-git fetch
-git reset origin/main
-git stash
